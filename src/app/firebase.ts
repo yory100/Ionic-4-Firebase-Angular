e72032b2b027a2c991ea612ff045055797b59b9d
yory <yory_man@yahoo.com>
2019-05-05 13:14:35 +0300

    first commits

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBRclXGjyPHmASI_t9EqKwyo8kmUH-W4vY',
+    authDomain: 'social-app-757ed.firebaseapp.com',
+    databaseURL: 'https://social-app-757ed.firebaseio.com',
+    projectId: 'social-app-757ed',
+    storageBucket: 'social-app-757ed.appspot.com',
+    messagingSenderId: '500006032660',
+    appId: '1:500006032660:web:5372b96c35d31e68'
+  };
+
+export default firebaseConfig;
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+ userName = '';
+  password = '';
+
+  constructor(public afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  async login() {
+    const { userName, password } = this;
+
+    try {
+
+      const res = await this.afAuth.auth.signInWithEmailAndPassword(userName + '@codedamn.com', password);
+
+    } catch (error) {
+      console.dir(error);
+      if (error.code === 'auth/user-not-found') {
+        console.log('User not found');
+      }
+    }
+  }
+
+}
